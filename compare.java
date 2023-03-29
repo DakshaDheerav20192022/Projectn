6fc01904c4cc82ea423559520c66bd4d031835eb
Appu Revu <nairrevathy09@gmail.com>
2023-03-29 17:59:15 +0530

    first commit

---
+++ compare.java
@@ -0,0 +1,15 @@
+package pkg1;
+//pgm2
+public class compare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=1000,b=570;
+boolean c=a>b;
+boolean d=b>a;
+System.out.println(""+c);
+System.out.println(""+d);
+
+	}
+
+}
